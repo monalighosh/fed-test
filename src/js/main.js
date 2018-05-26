9ad4329a73e03bf4c0957da9e4b0483d9e61607a
Monali Ghosh <monalighosh@users.noreply.github.com>
2018-05-26 19:04:48 +1000

    Updating card grid functionality

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,29 @@
 const url = "http://prototype.carter-dev.net/fed-test/items.json";
 const cardsContainer = document.querySelector(".cards-container");
 
+// Function to insert a SVG image in HTML doc
+function insertSvgImg(img, imgClass) {
+  return `<object type="image/svg+xml" data="images/${img}" class="${imgClass}">
+            Sorry! Your browser does not support SVG!
+        </object>`;
+}
+
+// Function to output cards in HTML doc
+function insertCards(title, id, description, documentSize) {
+  return `<div class="cards-container__item">
+            <h1 class="cards-container__item-title">${title}</h1>
+            <p class="cards-container__item-des">${id === "1" ? `${description}` : ``}</p>
+            <p class="cards-container__item-pdf">${documentSize ? `PDF (${documentSize})` : ``}</p>
+            <p class="cards-container__item-arrow"><a href="">
+              ${
+                documentSize
+                  ? insertSvgImg("arrow-down.svg", "img--arrow")
+                  : insertSvgImg("arrow-right.svg", "img--arrow")
+              }
+            </a></p>
+        </div>`;
+}
+
 // Create a XmlHttp request to fetch data
 const xhr = new XMLHttpRequest();
 // Open a XmlHttp request
@@ -15,23 +38,10 @@ xhr.onload = function() {
     let output = "";
     // Loop through cards array and update item info to card container div
     cards.forEach(card => {
-      output += `<div class="cards-container__item">
-        <p class="cards-container__item-title">${card.title}</p>
-        <p class="cards-container__item-des">${card.id === "1" ? `${card.description}` : ``}</p>
-        <p class="cards-container__item-pdf">${
-          card.documentSize ? `PDF (${card.documentSize})` : ``
-        }</p>
-        <p class="cards-container__item-arrow"><a href="">${
-          card.documentSize
-            ? `<img src="images/arrow-down.svg"/>`
-            : `<img src="images/arrow-right.svg"/>`
-        }</a></p>
-      </div>`;
+      output += insertCards(card.title, card.id, card.description, card.documentSize);
     });
     cardsContainer.innerHTML = output;
   }
 };
 // Send a XmlHttp request
 xhr.send();
-
-
