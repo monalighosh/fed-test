8b3c176c894bed8e051460e88601e3643d433900
Monali Ghosh <monalighosh@users.noreply.github.com>
2018-05-26 16:19:22 +1000

    Updating source files

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const url = "http://prototype.carter-dev.net/fed-test/items.json";
+const cardsContainer = document.querySelector(".cards-container");
+
+// Create a XmlHttp request to fetch data
+const xhr = new XMLHttpRequest();
+// Open a XmlHttp request
+xhr.open("GET", url);
+xhr.onload = function() {
+  // Check response status to determine whether the request was successful or not.
+  if (xhr.status === 200) {
+    // Store JSON response
+    const cards = JSON.parse(xhr.response).items;
+    let output = "";
+    // Loop through cards array and update item info to card container div
+    cards.forEach(card => {
+      output += `<div class="cards-container__item">
+        <p class="cards-container__item-title">${card.title}</p>
+        <p class="cards-container__item-des">${card.id === "1" ? `${card.description}` : ``}</p>
+      </div>`;
+    });
+    cardsContainer.innerHTML = output;
+  }
+};
+// Send a XmlHttp request
+xhr.send();
